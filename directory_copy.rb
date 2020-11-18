330b2225ec071b75a3c2efb687a7d1c7eee85924
Zeen Lam <zeen.lam93@gmail.com>
2020-11-18 17:54:05 +0000

    completed all exercises

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,26 +1,80 @@
+COHORT = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+]
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
   students = []
-  name = gets.chomp
+  name = gets.chomp.capitalize
+
+  puts "Enter cohort"
+  cohort = gets.chomp.capitalize
+  if cohort.empty?
+    cohort = :November
+  else
+    while !COHORT.include?(cohort)
+      puts "No such cohort, please try again."
+      cohort = gets.chomp.capitalize
+    end
+  end
+
+  puts "Enter Nationality"
+  nationality = gets.chomp.capitalize
   
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
+    students << {name: name, cohort: cohort.to_sym, nationality: nationality}
+    if students.count == 1
+      puts "Now we have 1 student"
+      puts "Please enter the names of the students"               
+    else
+      puts "Now we have #{students.count} students"
+      puts "Please enter the names of the students"
+      end
+      break
+    end
   students
 end
 
+def group_cohort(students)
+  students.sort_by{|student| student[:cohort]}
+end
+
 def print_by_letter(students)
-  students.each { |student| 
-    if student[:name][0] == 't' || student[:name][0] == 'T'
-      puts student[:name]
+  puts "Which letter would you like to filter?"
+  puts "Just hit return if you do not want to filter"
+  letter = gets.chomp.upcase
+  while !letter.empty? do
+    students.each { |student| 
+    if student[:name][0].include? letter
+      puts "#{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"
     else
       nil
     end
   }
+  break
+  end
+  
+end
+
+def less_than_12(students)
+  puts "Students with less than 12 characters"
+  students.each do 
+    |student| if student[:name].size < 12 then puts "#{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"
+    end
+  end
 end
 
 def print_header
@@ -30,7 +84,7 @@ end
 
 def print(students)
   students.each_with_index {|student, idx| 
-    puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort)" }
+    puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"}
 end
 
 def print_footer(students)
@@ -38,7 +92,10 @@ def print_footer(students)
 end
 
 students = input_students
-print_header
+students = group_cohort(students)
+print_header()
 print(students)
-print_footer(students)
 print_by_letter(students)
+less_than_12(students)
+print_footer(students)
+
