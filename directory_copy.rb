b84eda7ca62d36572734527069bc9b1e75d79619
Zeen Lam <zeen.lam93@gmail.com>
2020-11-19 14:41:58 +0000

    exercise complete

--- directory_copy.rb
+++ directory_copy.rb
@@ -3,7 +3,7 @@
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -17,6 +17,8 @@ def process(selection)
     filter_by_letter
   when "4"
     save_students
+  when "5"
+    load_students
   when "9"
     exit
   else
@@ -29,6 +31,7 @@ def print_menu
   puts "2. Show the students"
   puts "3. Filter students by letter"
   puts "4. Save the list to students.csv"
+  puts "5. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -52,24 +55,21 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp.capitalize
+  name = STDIN.gets.chomp.capitalize
 
   puts "Enter cohort"
-  cohort = gets.chomp.capitalize
+  cohort = STDIN.gets.chomp.capitalize
   if cohort.empty?
     cohort = :November
   else
     while !COHORT.include?(cohort)
       puts "No such cohort, please try again."
-      cohort = gets.chomp.capitalize
+      cohort = STDIN.gets.chomp.capitalize
     end
   end
-
-  puts "Enter Nationality"
-  nationality = gets.chomp.capitalize
   
   while !name.empty? do
-    @students << {name: name, cohort: cohort.to_sym, nationality: nationality}
+    @students << {name: name, cohort: cohort.to_sym}
     if @students.count == 1
       puts "Now we have 1 student"
       puts "Please enter the names of the students"               
@@ -87,13 +87,38 @@ def save_students
   file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
-    student_data = [student[:name], student[:cohort], student[:nationality]]
+    student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+def load_students(filename = "students.csv")
+  puts 'Please enter your files name'
+  filename = STDIN.gets.chomp
+  File.foreach(filename) do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort}
+  end
+  if @students.count >= 1
+    puts "#{@students}"
+  else 
+    puts "No students on file."
+  end
+end
 
 def filter_cohort()
   puts "Which cohort would you like to filter?"
@@ -153,6 +178,5 @@ def print_footer()
   puts "Overall, we have #{@students.count} great students"
 end
 
+try_load_students
 interactive_menu()
-
-
