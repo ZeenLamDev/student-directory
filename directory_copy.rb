4eb1ff28ee1736505b2e0ff6eddb061243d3136c
Zeen Lam <zeen.lam93@gmail.com>
2020-11-19 13:20:47 +0000

    added interactive_menu

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,3 +1,37 @@
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    filter_by_letter
+  when "4"
+
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Filter students by letter"
+  puts "4. "
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
 COHORT = [
   "January",
   "Febuary",
@@ -13,11 +47,11 @@ COHORT = [
   "December"
 ]
 
+# 1. Input_students
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp.capitalize
 
   puts "Enter cohort"
@@ -35,29 +69,39 @@ def input_students
   nationality = gets.chomp.capitalize
   
   while !name.empty? do
-    students << {name: name, cohort: cohort.to_sym, nationality: nationality}
-    if students.count == 1
+    @students << {name: name, cohort: cohort.to_sym, nationality: nationality}
+    if @students.count == 1
       puts "Now we have 1 student"
       puts "Please enter the names of the students"               
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
       puts "Please enter the names of the students"
       end
       break
     end
-  students
+  @students
 end
 
-def group_cohort(students)
-  students.sort_by{|student| student[:cohort]}
+def filter_cohort()
+  puts "Which cohort would you like to filter?"
+  cohort = gets.chomp.capitalize
+  while !cohort.empty? do
+    @students.each { |student| 
+    if student[:cohort][0].include? cohort
+      puts "#{@student[:name]} (#{@student[:cohort]} cohort) (#{@student[:nationality]})"
+    else
+      nil
+    end
+  }
+  break
+  end
 end
 
-def print_by_letter(students)
-  puts "Which letter would you like to filter?"
-  puts "Just hit return if you do not want to filter"
+def filter_by_letter()
+  puts "Which letter would you like to filter students by?"
   letter = gets.chomp.upcase
   while !letter.empty? do
-    students.each { |student| 
+    @students.each { |student| 
     if student[:name][0].include? letter
       puts "#{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"
     else
@@ -66,12 +110,11 @@ def print_by_letter(students)
   }
   break
   end
-  
 end
 
-def less_than_12(students)
+def less_than_12()
   puts "Students with less than 12 characters"
-  students.each do 
+  @students.each do 
     |student| if student[:name].size < 12 then puts "#{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"
     end
   end
@@ -82,20 +125,21 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index {|student, idx| 
+def print_students_list()
+  @students.each_with_index {|student, idx| 
     puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort) (#{student[:nationality]})"}
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
-students = input_students
-students = group_cohort(students)
-print_header()
-print(students)
-print_by_letter(students)
-less_than_12(students)
-print_footer(students)
+def print_footer()
+  puts "Overall, we have #{@students.count} great students"
+end
+
+interactive_menu()
+
 
